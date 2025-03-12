f28bfa9df7fa9622b5f308884252a2de87ce893a
Tiago Afonso <a20452@alunos.ipca.pt>
2025-03-12 15:28:03 +0000

    Referencia aos Posts do Utilizador

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
- 
- import org.springframework.data.annotation.Id;
- import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
  
  @Document(collection="user")
  public class User implements Serializable {
@@ -14,6 +17,9 @@ import java.io.Serializable;
  	private String name;
  	private String email;
  
+ 	@DBRef(lazy = true)
+ 	private List<Post> posts = new ArrayList<>();
+ 	
  	public User() {
  	}
  
@@ -47,6 +53,14 @@ import java.io.Serializable;
  	public void setEmail(String email) {
  		this.email = email;
  	}
+ 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
  
  	@Override
  	public int hashCode() {
@@ -72,4 +86,5 @@ import java.io.Serializable;
  			return false;
  		return true;
  	}
+
  }
