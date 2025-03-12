cb0ae9bbe15d4f8528ec883d672fdf619eb3cb72
Tiago Afonso <a20452@alunos.ipca.pt>
2025-03-12 09:58:16 +0000

    Projeto Spring Boot + Mongo DB

---
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.example.workshopmongo.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.UserDTO;
+import com.example.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+
+		List<User> list = service.findAll();
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+}
