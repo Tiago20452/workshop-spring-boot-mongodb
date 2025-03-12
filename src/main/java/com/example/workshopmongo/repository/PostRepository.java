a261a660eca2cf74a027b41a5ac09f73c2b9f3a8
Tiago Afonso <a20452@alunos.ipca.pt>
2025-03-12 16:06:51 +0000

    Consulta simples com Query

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
+import java.util.List;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
